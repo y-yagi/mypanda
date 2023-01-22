618016e4c7da2dd646cb154294d406686f0b977e
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2023-01-22 15:55:56 +0900

    Extract a component

---
+++ src/components/feeds.jsx
@@ -0,0 +1,18 @@
+const Feeds = ({ feeds }) => {
+    return (
+        <>
+            {feeds.map((feed) => (
+                <div className="max-w-sm p-1 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 text-left mb-1">
+                    <a href={feed.link} target="_blank" rel="noopener noreferrer" >
+                        <h5 className="mb-1 hover:underline text-gray-900">{feed.title}</h5>
+                    </a>
+                    <a href={feed.comments} target="_blank" rel="noopener noreferrer" className="text-sm inline-flex items-right text-gray-500 hover:underline">
+                        Comments
+                    </a>
+                </div>
+            ))}
+        </>
+    );
+};
+
+export default Feeds;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import "../components/feeds";
+import Feeds from "../components/feeds";
 
 function App() {
   const [hackewnewsFeeds, setHackewnewsFeeds] = useState("");
@@ -23,14 +25,9 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
-  const parsedHackewnewsFeeds = () => {
-    if (hackewnewsFeeds.length === 0 ) return [];
-    return JSON.parse(hackewnewsFeeds);
-  }
-
-  const parsedReddtFeeds = () => {
-    if (redditFeeds.length === 0 ) return [];
-    return JSON.parse(redditFeeds);
+  const parseFeeds = (feeds) => {
+    if (feeds.length === 0 ) return [];
+    return JSON.parse(feeds);
   }
 
   return (
@@ -39,29 +36,11 @@ function App() {
       <div className="flex flex-row">
         <div className="flex-2 mb-10 mr-10">
           <h4 className="font-bold text-[#ff6600]">Hacker News</h4>
-          {parsedHackewnewsFeeds().map((feed) => (
-            <div className="max-w-sm p-1 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 text-left mb-1">
-              <a href={feed.link} target="_blank" rel="noopener noreferrer" >
-                <h5 className="mb-1 hover:underline text-gray-900">{feed.title}</h5>
-              </a>
-              <a href={feed.comments} target="_blank" rel="noopener noreferrer" className="text-sm inline-flex items-right text-gray-500 hover:underline">
-                Comments
-              </a>
-            </div>
-          ))}
+          <Feeds feeds={parseFeeds(hackewnewsFeeds)} />
         </div>
         <div className="flex-2 mb-10 mr-10">
           <h4 className="font-bold text-[#ff4500]">Reddit</h4>
-          {parsedReddtFeeds().map((feed) => (
-            <div className="max-w-sm p-1 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 text-left mb-1">
-              <a href={feed.link} target="_blank" rel="noopener noreferrer" >
-                <h5 className="mb-1 hover:underline text-gray-900">{feed.title}</h5>
-              </a>
-              <a href={feed.comments} target="_blank" rel="noopener noreferrer" className="text-sm inline-flex items-right text-gray-500 hover:underline">
-                Comments
-              </a>
-            </div>
-          ))}
+          <Feeds feeds={parseFeeds(redditFeeds)} />
         </div>
       </div>
     </div>
