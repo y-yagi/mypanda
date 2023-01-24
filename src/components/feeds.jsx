26197be4ccf4f779f158b074e1e6a0475f94624d
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2023-01-24 12:27:15 +0900

    Adjust design

--- src/components/feeds.jsx
+++ src/components/feeds.jsx
@@ -2,15 +2,15 @@ const Feeds = ({ feeds }) => {
     return (
         <>
             {feeds.map((feed) => (
-                <div className="max-w-sm p-1 bg-white border border-gray-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 text-left mb-1">
+                <div className="max-w-sm p-1 bg-white border border-rose-200 rounded-lg shadow-md dark:bg-gray-800 dark:border-gray-700 text-left mb-1">
                     <a href={feed.link} target="_blank" rel="noopener noreferrer" >
-                        <h5 className="mb-1 hover:underline text-gray-900">{feed.title}</h5>
+                        <h5 className="text-sm mb-1 hover:underline text-gray-900">{feed.title}</h5>
                     </a>
                     {feed.description.length > 0 && feed.comments.length === 0 &&
-                        <p className="text-sm inline-flex items-right text-gray-500">{feed.description}</p>
+                        <p className="text-xs inline-flex items-right text-gray-500">{feed.description}</p>
                     }
                     {feed.comments.length > 0 &&
-                        <a href={feed.comments} target="_blank" rel="noopener noreferrer" className="text-sm inline-flex items-right text-gray-500 hover:underline">
+                        <a href={feed.comments} target="_blank" rel="noopener noreferrer" className="text-xs inline-flex items-right text-gray-500 hover:underline">
                             Comments
                         </a>
                     }
